6f02d47e0c8f8f3d0db29b6c10536c7774d2ef45
dearmadman <i@dearmadman.com>
2016-09-16 15:55:36 +0800

    update

--- build/en.sh
+++ build/en.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 base=/home/www/docs.larastars.cn
 docs=${base}/resources/docs/en
+origin=https://github.com/laravel/docs
+
+function update ()
+{
+  version=$1
+  rm -rf ${docs}/${version} && cd ${docs}/ && git clone ${origin} -b ${version} ${version} && cd ${version} && sed -i "s/\/docs\/{{version}}/\/{{language}}\/{{version}}/g" `ls`
+}
 
 # cd ${docs}/5.0 && git pull origin 5.0
 # cd ${docs}/5.1 && git pull origin 5.1
 # cd ${docs}/5.2 && git pull origin 5.2
-cd ${docs}/5.3 && git pull origin 5.3
+update 5.3
 # cd ${docs}/master && git pull origin master
 
 cd $base && php artisan docs:clear-cache
